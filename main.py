543a3b0779d1331ff39ba1091636f9400ae0557d
Mahmoud Adel <infinite.group11@outlook.com>
2023-10-07 17:28:27 +0300

    new codee

--- main.py
+++ main.py
@@ -1,29 +1,36 @@
 import socket
 import sys
+import threading
+
+def handle_client(client_socket, client_address):
+    try:
+        while True:
+            data = client_socket.recv(1024)
+            if not data:
+                break
+            print(f"Received data from {client_address}: {data.decode('utf-8')}")
+            sys.stdout.flush()
+            
+            # Continue listening for the next message
+    except Exception as e:
+        print(f"Error handling client {client_address}: {e}")
+    finally:
+        client_socket.close()
+        print(f"Connection with {client_address} closed.")
 
 def start_server(host, port):
     try:
         server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         server_socket.bind((host, port))
         server_socket.listen(5)
+        print(f"Server listening on {host}:{port}")
+
         while True:
             client_socket, client_address = server_socket.accept()
-            try:
-                client_socket.settimeout(25)  # Set a 25-second timeout for receiving data
-                data = client_socket.recv(1024)
-                if not data:
-                    break
-                print(f"Received data from {client_address}: {data.decode('utf-8')}")
-                sys.stdout.flush()
-            except socket.timeout:
-                print(f"No data received from {client_address} within 25 seconds. Closing connection.")
-                sys.stdout.flush()
-                client_socket.close()
-            except Exception as e:
-                print(f"Error handling client {client_address}: {e}")
-                sys.stdout.flush()
-                client_socket.close()
-                
+            print(f"Accepted connection from {client_address}")
+            
+            client_thread = threading.Thread(target=handle_client, args=(client_socket, client_address))
+            client_thread.start()
     except Exception as e:
         print(f"Error starting server: {e}")
         sys.stdout.flush()
