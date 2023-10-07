866f26e2aa65be0d61fc37ea120158edade75a81
Mahmoud Adel <infinite.group11@outlook.com>
2023-10-07 16:19:07 +0300

    handel connection timeout

--- main.py
+++ main.py
@@ -1,63 +1,37 @@
 import socket
-import logging
-import threading
-import json
+import sys
 
-# Configure logging
-logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-logger = logging.getLogger('TCP_Server')
-
-def handle_client(client_socket, client_address):
+def start_server(host, port):
     try:
-        logger.info(f"Accepted connection from {client_address}")
-
-        while True:
-            # Receive data from the client
-            data = client_socket.recv(1024)
-            if not data:
-                break
-
-            # Log the received data
-            logger.info(f"Received data from {client_address}: {data.decode('utf-8').strip()}")
-
-            # Send a response back to the client (optional)
-            # client_socket.send(b"Data received")
-
-    except Exception as e:
-        logger.error(f"Error handling client {client_address}: {e}")
-    finally:
-        # Close the client socket
-        client_socket.close()
-        logger.info(f"Connection with {client_address} closed")
-
-def start_tcp_server(host, port):
-    try:
-        # Create a socket object
         server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
-        # Bind the socket to the address and port
         server_socket.bind((host, port))
-
-        # Listen for incoming connections
         server_socket.listen(5)
-        logger.info(f"Listening on {host}:{port}")
-
         while True:
-            # Accept a connection from a client
             client_socket, client_address = server_socket.accept()
-
-            # Start a new thread to handle the client
-            client_thread = threading.Thread(target=handle_client, args=(client_socket, client_address))
-            client_thread.start()
-
+            try:
+                client_socket.settimeout(10)  # Set a 10-second timeout for receiving data
+                data = client_socket.recv(1024)
+                if not data:
+                    break
+                print(f"Received data from {client_address}: {data.decode('utf-8')}")
+            except socket.timeout:
+                print(f"No data received from {client_address} within 5 seconds. Closing connection.")
+            except Exception as e:
+                print(f"Error handling client {client_address}: {e}")
+            finally:
+                client_socket.close()
     except Exception as e:
-        logger.error(f"Error starting server: {e}")
+        print(f"Error starting server: {e}")
     finally:
-        # Close the server socket
         server_socket.close()
 
 if __name__ == "__main__":
-    host = "0.0.0.0"  # Listen on all available network interfaces
-    port = 8080  # Use port 8080
-
-    start_tcp_server(host, port)
+    host = "0.0.0.0"
+    port = 8080
+    try:
+        start_server(host, port)
+    except KeyboardInterrupt:
+        print("\nServer terminated by user.")
+        sys.exit(0)
+    except Exception as e:
+        print(f"Server error: {e}")
