a8c1a8ce1a86369156c3c28a97247c1aca5a0e3e
Mahmoud Adel <infinite.group11@outlook.com>
2023-10-07 11:41:32 +0300

    edit the tcp server

--- main.py
+++ main.py
@@ -1,55 +1,67 @@
 import socket
+import logging
 import threading
-import sys
 
-# Define the server's host and port
-HOST = '0.0.0.0'  # Listen on all available network interfaces
-PORT = 8080      # Choose a port for your server (e.g., 8080)
-
-def main():
-    # Create a TCP socket
-    try:
-        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as server_socket:
-            # Bind the socket to the host and port
-            server_socket.bind((HOST, PORT))
-
-            # Listen for incoming connections
-            server_socket.listen()
-
-            sys.stdout.write(f"Server is listening on {HOST}:{PORT}\n")
-            sys.stdout.flush()
-
-            while True:
-                # Accept a client connection
-                client_socket, client_address = server_socket.accept()
-                sys.stdout.write(f"Accepted connection from {client_address}\n")
-                sys.stdout.flush()
-
-                # Create a new thread to handle the client connection
-                client_thread = threading.Thread(target=handle_client, args=(client_socket, client_address))
-                client_thread.start()
-    except Exception as e:
-        sys.stderr.write(f"An error occurred: {e}\n")
-        sys.stderr.flush()
-        sys.exit(1)
+# Configure logging
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+logger = logging.getLogger('TCP_Server')
 
 def handle_client(client_socket, client_address):
-    with client_socket:
-        try:
-            while True:
-                # Receive data from the client
-                data = client_socket.recv(1024)
+    try:
+        logger.info(f"Accepted connection from {client_address}")
 
-                if not data:
-                    break  # No more data from the client, disconnect
+        while True:
+            # Receive data from the client
+            data = client_socket.recv(1024)
+            if not data:
+                break
 
-                # Process the received data (customize this part for your application)
-                # For now, we'll just echo the data back to the client
-                sys.stdout.write(f"Received data from {client_address}: {data.decode('utf-8')}\n")
-                sys.stdout.flush()
-        except Exception as e:
-            sys.stderr.write(f"An error occurred while handling the client connection: {e}\n")
-            sys.stderr.flush()
+            # Decode the received data and split it into headers and body
+            data_str = data.decode('utf-8')
+            headers, body = data_str.split('\r\n\r\n', 1)
+
+            # Log the headers and the received data
+            logger.info(f"Received headers from {client_address}:\n{headers}")
+            logger.info(f"Received data from {client_address}:\n{body}")
+
+            # Send a response back to the client (optional)
+            # client_socket.send(b"Data received")
+
+    except Exception as e:
+        logger.error(f"Error handling client {client_address}: {e}")
+    finally:
+        # Close the client socket
+        client_socket.close()
+        logger.info(f"Connection with {client_address} closed")
+
+def start_tcp_server(host, port):
+    try:
+        # Create a socket object
+        server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+        # Bind the socket to the address and port
+        server_socket.bind((host, port))
+
+        # Listen for incoming connections
+        server_socket.listen(5)
+        logger.info(f"Listening on {host}:{port}")
+
+        while True:
+            # Accept a connection from a client
+            client_socket, client_address = server_socket.accept()
+
+            # Start a new thread to handle the client
+            client_thread = threading.Thread(target=handle_client, args=(client_socket, client_address))
+            client_thread.start()
+
+    except Exception as e:
+        logger.error(f"Error starting server: {e}")
+    finally:
+        # Close the server socket
+        server_socket.close()
 
 if __name__ == "__main__":
-    main()
+    host = "127.0.0.1"  # Change to your desired host
+    port = 8888  # Change to your desired port
+
+    start_tcp_server(host, port)
