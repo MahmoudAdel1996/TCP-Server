79b831a351aec3dc22cbe7be1a7664308338a40f
Mahmoud Adel <infinite.group11@outlook.com>
2023-10-08 12:56:54 +0300

    edit print message

--- main.py
+++ main.py
@@ -10,9 +10,6 @@ def handle_client(client_socket, client_address):
         # client_socket.settimeout(CLIENT_SOCKET_TIMEOUT)
         while True:
             data = client_socket.recv(1024 * 1024)
-            if not data:
-                print("empty data sent")
-                sys.stdout.flush()
             print(f"Received data from {client_address}: {data.decode('utf-8')}")
             sys.stdout.flush()
             ack_message = "Data received and processed successfully."
