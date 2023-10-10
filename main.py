519ddfc7d811b13aedb636f25b4122559eaa0531
Mahmoud Adel <infinite.group11@outlook.com>
2023-10-11 02:17:19 +0300

    increase buffer size

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ def handle_client(client_socket, client_address):
     try:
         client_socket.settimeout(CLIENT_SOCKET_TIMEOUT)
         while True:
-            data = client_socket.recv(1024)
+            data = client_socket.recv(1024 * 1024)
             if not data:
                 break
             try:
