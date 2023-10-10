4e50035a13b5b05e2ca71d7c1c6be58471f7e363
Mahmoud Adel <infinite.group11@outlook.com>
2023-10-11 02:08:58 +0300

    improve code

--- main.py
+++ main.py
@@ -1,31 +1,29 @@
 import socket
 import sys
-import threading
+import asyncio
 
+CLIENT_SOCKET_TIMEOUT = 60
 
-# CLIENT_SOCKET_TIMEOUT = 25
-
-def handle_client(client_socket, client_address):
+async def handle_client(client_socket, client_address):
     try:
-        # client_socket.settimeout(CLIENT_SOCKET_TIMEOUT)
+        client_socket.settimeout(CLIENT_SOCKET_TIMEOUT)
         while True:
-            data = client_socket.recv(1024 * 1024)
+            data = await loop.sock_recv(client_socket, 4096)
             if not data:
                 break
             try:
                 decoded_data = data.decode('utf-8')
                 print(f"Received data from {client_address}: {decoded_data}")
             except UnicodeDecodeError:
-                # Handle decoding errors (e.g., logging, skipping, or converting to another encoding)
                 print(f"Error decoding data from {client_address}")
             sys.stdout.flush()
 
             ack_message = "Data received and processed successfully.\n"
-            client_socket.send(ack_message.encode('utf-8'))
+            await loop.sock_sendall(client_socket, ack_message.encode('utf-8'))
 
     except ConnectionResetError:
         print(f"Connection reset by {client_address}")
-        sys.stdout.flush()  
+        sys.stdout.flush()
     except socket.timeout:
         print(f"Client connection timed out")
         sys.stdout.flush()
@@ -37,7 +35,7 @@ def handle_client(client_socket, client_address):
         print(f"Connection with {client_address} closed.")
         sys.stdout.flush()
 
-def start_server(host, port):
+async def start_server(host, port):
     try:
         server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         server_socket.bind((host, port))
@@ -48,8 +46,7 @@ def start_server(host, port):
             client_socket, client_address = server_socket.accept()
             print(f"Accepted connection from {client_address}")
             sys.stdout.flush()
-            client_thread = threading.Thread(target=handle_client, args=(client_socket, client_address))
-            client_thread.start()
+            await loop.run_in_executor(None, handle_client, client_socket, client_address)
     except Exception as e:
         print(f"Error starting server: {e}")
         sys.stdout.flush()
@@ -59,12 +56,15 @@ def start_server(host, port):
 if __name__ == "__main__":
     host = "0.0.0.0"
     port = 8080
+    loop = asyncio.get_event_loop()
     try:
-        start_server(host, port)
+        loop.run_until_complete(start_server(host, port))
     except KeyboardInterrupt:
         print("\nServer terminated by user.")
         sys.stdout.flush()
-        sys.exit(0)
+        loop.run_until_complete(loop.shutdown_asyncgens())
     except Exception as e:
         print(f"Server error: {e}")
         sys.stdout.flush()
+    finally:
+        loop.close()
