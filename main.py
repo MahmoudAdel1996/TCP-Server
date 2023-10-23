687b93ca2dd7b138381a6ba77619a1306d5adf78
Mahmoud Adel <Mahmoud.Adel@urwave.com>
2023-10-24 01:12:39 +0300

    accept multi socket

--- main.py
+++ main.py
@@ -1,8 +1,15 @@
 import socket
 import sys
+import threading
 
 CLIENT_SOCKET_TIMEOUT = 60
 BUFFER_SIZE = 4096
+
+# Lock for protecting shared resources (if any)
+lock = threading.Lock()
+
+# List to keep track of active client threads
+active_threads = []
 
 def handle_client(client_socket, client_address):
     try:
@@ -31,9 +38,13 @@ def handle_client(client_socket, client_address):
         print(f"Error handling client {client_address}: {e}")
         sys.stdout.flush()
     finally:
+        # Close the client socket
         client_socket.close()
         print(f"Connection with {client_address} closed.")
         sys.stdout.flush()
+        # Remove the current thread from the active_threads list
+        with lock:
+            active_threads.remove(threading.current_thread())
 
 def start_server(host, port):
     try:
@@ -46,12 +57,25 @@ def start_server(host, port):
             client_socket, client_address = server_socket.accept()
             print(f"Accepted connection from {client_address}")
             sys.stdout.flush()
-            handle_client(client_socket, client_address)
+
+            # Create a new thread to handle the client
+            client_thread = threading.Thread(target=handle_client, args=(client_socket, client_address))
+            client_thread.start()
+            # Add the new thread to the active_threads list
+            with lock:
+                active_threads.append(client_thread)
+
     except Exception as e:
         print(f"Error starting server: {e}")
         sys.stdout.flush()
     finally:
         server_socket.close()
+
+def stop_server():
+    # Stop all active threads
+    with lock:
+        for thread in active_threads:
+            thread.join()
 
 if __name__ == "__main__":
     host = "0.0.0.0"
@@ -61,7 +85,9 @@ if __name__ == "__main__":
     except KeyboardInterrupt:
         print("\nServer terminated by user.")
         sys.stdout.flush()
+        stop_server()  # Stop all active threads
         sys.exit(0)
     except Exception as e:
         print(f"Server error: {e}")
+        stop_server()  # Stop all active threads
         sys.stdout.flush()
